cca0b7632196379ad840029d4e17a808164af5e5
sebaurel <sebaurel@free.fr>
2018-04-05 21:35:44 +0200

    La quete sur les variables

---
+++ src/fr/wildeCodeSchool/variable/IndianaJones.java
@@ -0,0 +1,12 @@
+package fr.wildeCodeSchool.variable;
+
+public class IndianaJones {
+    public static void main(String... args){
+        String originalTitle = "Indiana Jones and the Last Crusade ";
+        boolean iSawIt = true;
+        int releaseYear = 1989;
+        double rate = 8.3;
+
+        System.out.println("Titre original : " + originalTitle + ".\nJe l'ai vu : " + iSawIt + ".\nAnnée de sortie : " + releaseYear + ".\nNote : " + rate + ".");
+    }
+}
